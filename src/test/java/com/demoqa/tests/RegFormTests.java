d84712f8cb179df7f2ea53e4a7b7f5f1a9563f3e
nesquik60 <red.polymorph@gmail.com>
2022-05-06 21:42:35 +0600

    Добавил attachments

--- src/test/java/com/demoqa/tests/RegFormTests.java
+++ src/test/java/com/demoqa/tests/RegFormTests.java
@@ -6,45 +6,71 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
 @Owner("Igor Kuznetsov")
 @Feature("Тест заполнения формы Demoqa")
 public class RegFormTests extends TestBase{
     RegFormPage form = new RegFormPage();
     RegFormTestData data = new RegFormTestData();
     RegFormPage checks = new RegFormPage();
-
-
     @Test
     @DisplayName("Заполнение формы")
     void positiveTest() {
         //set values in the table
-        form
-                .openPage()
-                .setFirstName(data.getFirstName())
-                .setLastName(data.getLastName())
-                .setEmail(data.getEmail())
-                .setGender(data.getGender())
-                .setPhone(data.getPhone())
-                .setBirthDay(data.getBirthDay())
-                .setSubject(data.getSubject())
-                .setHobbies(data.getHobby())
-                .setPicture(data.getPathToPicture())
-                .setAddress(data.getAddress())
-                .setStateAndCity(data.getState(), data.getCity())
-                .clickSubmitButton();
+        step("Открываем страницу с формой", () -> {
+            form.openPage();
+        });
+        step("Задаем им", () -> {
+            form.setFirstName(data.getFirstName());
+        });
+        step("Задаем фамилию", () -> {
+            form.setLastName(data.getLastName());
+        });
+        step("Задаем emaik", () -> {
+            form.setEmail(data.getEmail());
+        });
+        step("Выбираем гендер", () -> {
+            form.setGender(data.getGender());
+        });
+        step("Задаем телефон", () -> {
+            form.setPhone(data.getPhone());
+        });
+        step("Задаем дату рождения", () -> {
+            form.setBirthDay(data.getBirthDay());
+        });
+        step("Задаем темы", () -> {
+            form.setSubject(data.getSubject());
+        });
+        step("Задаем хобби", () -> {
+            form.setHobbies(data.getHobby());
+        });
+        step("Загружаем картинку", () -> {
+            form.setPicture(data.getPathToPicture());
+        });
+        step("Задаем адрес", () -> {
+            form.setAddress(data.getAddress());
+        });
+        step("Задаем штат и город", () -> {
+            form.setStateAndCity(data.getState(), data.getCity());
+        });
+        step("Нажимаем на кнопку Submit", () -> form.clickSubmitButton());
 
         //assertions
-        checks
-                .checkResulnTables("Student Name", data.getExpectedFullName())
-                .checkResulnTables("Student Email", data.getEmail())
-                .checkResulnTables("Gender", data.getGender())
-                .checkResulnTables("Mobile", data.getPhone())
-                .checkResulnTables("Date of Birth", data.getExpectedBirthDate())
-                .checkResulnTables("Subjects", data.getSubject())
-                .checkResulnTables("Hobbies", data.getHobby())
-                .checkResulnTables("Picture", data.getPictuteName())
-                .checkResulnTables("Address", data.getAddress())
-                .checkResulnTables("State and City", data.getExpectedStateAndCity())
-                .clickCloseButton();
+        step("Проверка таблицы с результатом заполнения", () -> {
+            checks
+                    .checkResulnTables("Student Name", data.getExpectedFullName())
+                    .checkResulnTables("Student Email", data.getEmail())
+                    .checkResulnTables("Gender", data.getGender())
+                    .checkResulnTables("Mobile", data.getPhone())
+                    .checkResulnTables("Date of Birth", data.getExpectedBirthDate())
+                    .checkResulnTables("Subjects", data.getSubject())
+                    .checkResulnTables("Hobbies", data.getHobby())
+                    .checkResulnTables("Picture", data.getPictuteName())
+                    .checkResulnTables("Address", data.getAddress())
+                    .checkResulnTables("State and City", data.getExpectedStateAndCity());
+        });
+        step("Закрываем таблицу с результатом заполнения", ()-> checks.clickCloseButton());
     }
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,15 +1,38 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.utils.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
     static void configurationTests() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
         Configuration.holdBrowserOpen = false; //true = не закрывать браузер после тестов
         Configuration.browserSize = "1700x920";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserPosition = "0x0";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+    @AfterEach
+    void addAttachement() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
